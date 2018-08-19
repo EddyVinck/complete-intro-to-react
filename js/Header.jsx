c0881716a0d93bed822e63a5ac5dc73423127075
EddyVinck <eddyvinck95@gmail.com>
2018-08-19 16:34:14 +0200

    fixed focus

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { bool, string, func } from 'prop-types';
 
-const Header = props => {
-  let utilSpace;
-  if (props.showSearch) {
-    utilSpace = (
-      <input
-        key="searchShow"
-        onChange={props.handleSearchTermChange}
-        value={props.searchTerm}
-        type="text"
-        placeholder="search"
-      />
-    );
-  } else {
-    utilSpace = <h2><Link to="/search">Back</Link></h2>;
+class Header extends Component {
+  componentDidMount() {
+    this.searchInput.focus();
+    const val = this.searchInput.value;
+    this.searchInput.value = '';
+    this.searchInput.value = val;
   }
-  return (
-    <header>
-      <h1><Link to="/">svideo</Link></h1>
-      {utilSpace}
-    </header>
-  );
-};
+  render() {
+    let utilSpace;
+    if (this.props.showSearch) {
+      utilSpace = (
+        <input
+          ref={input => {
+            this.searchInput = input;
+          }}
+          key="searchShow"
+          onChange={this.props.handleSearchTermChange}
+          value={this.props.searchTerm}
+          type="text"
+          placeholder="search"
+        />
+      );
+    } else {
+      utilSpace = <h2><Link to="/search">Back</Link></h2>;
+    }
+    return (
+      <header>
+        <h1><Link to="/">svideo</Link></h1>
+        {utilSpace}
+      </header>
+    );
+  }
+}
 
 Header.defaultProps = {
   showSearch: false,
